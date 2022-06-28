a303933c0680a92472ae0d1baf2ce5b8f4012fb7
XAIMI-Lab <101001905+XAIMI-Lab@users.noreply.github.com>
2022-06-28 10:49:35 +0800

    Create cartesian_dataset.py

--- datasets/cartesian_dataset.py
+++ datasets/cartesian_dataset.py
@@ -2,7 +2,7 @@ import os
 import scipy.io as sio
 import torch.utils.data as data
 from datasets.utilizes import *
-from models.utils import fft2, ifft2, to_tensor
+from models.utils import to_tensor,FFT2D
 
 class MRIDataset_Cartesian(data.Dataset):
     def __init__(self, opts, mode):
@@ -18,6 +18,10 @@ class MRIDataset_Cartesian(data.Dataset):
             self.seed = 1234
 
         if self.mode == 'TEST':
+            self.data_dir_flair = os.path.join(opts.data_root, 'test')
+            self.sample_list = open(os.path.join(opts.list_dir, self.mode + '.txt')).readlines()
+            self.seed = 5678
+        if self.mode == 'TEST_Tumor':
             self.data_dir_flair = os.path.join(opts.data_root, 'test')
             self.sample_list = open(os.path.join(opts.list_dir, self.mode + '.txt')).readlines()
             self.seed = 5678
@@ -38,17 +42,25 @@ class MRIDataset_Cartesian(data.Dataset):
         T2_256_img_real = T2_256_img_real[np.newaxis, :, :]
         T2_256_img_imag = T2_img['T2'].imag
         T2_256_img_imag = T2_256_img_imag[np.newaxis, :, :]
+
+        T2_256_img_real = (T2_256_img_real + 1) / 2
+        T2_256_img_imag = (T2_256_img_imag + 1) / 2
+
         T2_256_img = np.concatenate([T2_256_img_real, T2_256_img_imag], axis=0)  # 2,w,h
         T2_256_img = to_tensor(T2_256_img).float()
         # =======
-        T2_256_img_k = T2_256_img.permute(1, 2, 0)
-        T2_256_img_k_ks = fft2(T2_256_img_k)
+        T2_256_img_k = T2_256_img.permute(1, 2, 0).contiguous()
+        T2_256_img_k_ks = torch.view_as_real(FFT2D(torch.view_as_complex(T2_256_img_k))).contiguous()
         T2_256_img_ks = T2_256_img_k_ks.permute(2, 0, 1)
         #=======
         T2_128_img_real = T2_img['T2_128'].real#ZF
         T2_128_img_real = T2_128_img_real[np.newaxis,:, :]
         T2_128_img_imag = T2_img['T2_128'].imag
         T2_128_img_imag = T2_128_img_imag[np.newaxis,:, :]
+
+        T2_128_img_real = (T2_128_img_real + 1) / 2
+        T2_128_img_imag = (T2_128_img_imag + 1) / 2
+
         T2_128_img = np.concatenate([T2_128_img_real, T2_128_img_imag], axis=0)#2,w,h
         T2_128_img = to_tensor(T2_128_img).float()
         #=======
@@ -56,6 +68,10 @@ class MRIDataset_Cartesian(data.Dataset):
         T2_64_real = T2_64_real[np.newaxis, :, :]
         T2_64_imag = T2_img['T2_64'].imag
         T2_64_imag = T2_64_imag[np.newaxis, :, :]
+
+        T2_64_real = (T2_64_real + 1) / 2
+        T2_64_imag = (T2_64_imag + 1) / 2
+
         T2_64_img = np.concatenate([T2_64_real, T2_64_imag], axis=0)
         T2_64_img = to_tensor(T2_64_img).float()
 
@@ -65,6 +81,10 @@ class MRIDataset_Cartesian(data.Dataset):
         T1_256_real = T1_256_real[np.newaxis, :, :]
         T1_256_imag = T2_img['T1'].imag
         T1_256_imag = T1_256_imag[np.newaxis, :, :]
+
+        T1_256_real = (T1_256_real + 1) / 2
+        T1_256_imag = (T1_256_imag + 1) / 2
+
         T1_256_img = np.concatenate([T1_256_real, T1_256_imag], axis=0)
         T1_256_img = to_tensor(T1_256_img).float()
         # =======T1 128
@@ -72,6 +92,10 @@ class MRIDataset_Cartesian(data.Dataset):
         T1_128_real = T1_128_real[np.newaxis, :, :]
         T1_128_imag = T2_img['T1_128'].imag
         T1_128_imag = T1_128_imag[np.newaxis, :, :]
+
+        T1_128_real = (T1_128_real + 1) / 2
+        T1_128_imag = (T1_128_imag + 1) / 2
+
         T1_128_img = np.concatenate([T1_128_real, T1_128_imag], axis=0)
         T1_128_img = to_tensor(T1_128_img).float()
         # =======T1 64
@@ -79,16 +103,20 @@ class MRIDataset_Cartesian(data.Dataset):
         T1_64_real = T1_64_real[np.newaxis, :, :]
         T1_64_imag = T2_img['T1_64'].imag
         T1_64_imag = T1_64_imag[np.newaxis, :, :]
+
+        T1_64_real = (T1_64_real + 1) / 2
+        T1_64_imag = (T1_64_imag + 1) / 2
+
         T1_64_img = np.concatenate([T1_64_real, T1_64_imag], axis=0)
         T1_64_img = to_tensor(T1_64_img).float()
         #=======
 
 # ---------------------over------
         return {'ref_image_full': T1_256_img,
-                'ref_image_sub': T1_64_img, #  if 2x, change it to T1_128_img
+                'ref_image_sub': T1_128_img, #  if 2x, change it to T1_128_img
                 'tag_image_full': T2_256_img,
                 'tag_kspace_full':T2_256_img_ks,
-                'tag_image_sub': T2_64_img, # if 2x, change it to T2_128_img
+                'tag_image_sub': T2_128_img, # if 2x, change it to T2_128_img
                 'tag_kspace_mask2d': mask  #  dc dc_mask
                 }
 
